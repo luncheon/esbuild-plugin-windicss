ad2afb7c2bf3deea0c62b66ec6c9150bc170a868
luncheon <takeuck+github@gmail.com>
2023-08-23 19:10:52 +0900

    update dependencies

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -51,7 +55,7 @@ const plugin = ({ filter, babelParserOptions, windiCssConfig } = {}) => {
             windiCss = new windicss_1.default(windiCssConfig);
         }
         const styleSheet = new style_1.StyleSheet();
-        for (const token of parser_1.parse(contents, resolvedBabelParserOptions).tokens) {
+        for (const token of (0, parser_1.parse)(contents, resolvedBabelParserOptions).tokens) {
             if (token.value && (token.type.label === 'string' || token.type.label === 'template')) {
                 const interpreted = windiCss.interpret(token.value.replace(ignoredClassPattern, ' ').trim(), true);
                 if (interpreted.success.length !== 0) {
@@ -72,7 +76,7 @@ const plugin = ({ filter, babelParserOptions, windiCssConfig } = {}) => {
             if (pipe?.transform) {
                 return transform(pipe.transform);
             }
-            build.onLoad({ filter: filter ?? /\.[jt]sx?$/ }, async (args) => transform({ args, contents: await promises_1.readFile(args.path, 'utf8') }));
+            build.onLoad({ filter: filter ?? /\.[jt]sx?$/ }, async (args) => transform({ args, contents: await (0, promises_1.readFile)(args.path, 'utf8') }));
             build.onResolve({ filter: RegExp(String.raw `\.${pluginName}\.css`) }, ({ path }) => ({ path, namespace: pluginName }));
             build.onLoad({ filter: RegExp(String.raw `\.${pluginName}\.css`), namespace: pluginName }, ({ path }) => {
                 const contents = cssFileContentsMap.get(path);
